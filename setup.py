22b83471cf9b261e77e2a08d4c89b77a367760af
mac <mac@MacBook-Pro-de-mac.local>
2024-12-13 19:46:09 +0100

    Setup.py file updated

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
+
+
 from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
     """
-    Thiss function will return list of requirements
+    This function will return list of requirements
     
     """
     requirement_lst:List[str]=[]
